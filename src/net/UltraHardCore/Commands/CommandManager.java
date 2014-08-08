125a13f1933fb9281484a41c121d378da1da50e4
CrazymanJR <adilkarim101@gmail.com>
2014-08-08 11:27:40 -0700

    Update CommandManager.java

--- src/net/UltraHardCore/Commands/CommandManager.java
+++ src/net/UltraHardCore/Commands/CommandManager.java
@@ -1,20 +1,17 @@
 package net.UltraHardCore.Commands.CommandManager;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.Command;
 
 public class CommandManager implements CommandExecutor {
   
-  private HashMap<String, SubCommand> cmds;
+  private ArrayList<String> cmds; //Might Have Made a mistake here, was on my phone doing this part
   
-  public CommandManager() {
-    this.cmds = new HashMap();
-    loadCommands();
-  }
+ 
   
-  private void loadCommands() {
+  protected CommandManager() {
     /*
     Join Command
     AddTeam Command
@@ -23,6 +20,26 @@ public class CommandManager implements CommandExecutor {
     
     -Add more if needed
     */
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    if (!(sender instanceof Player)) {
+      sender.sendMessage("Only players can do this");
+    }
+    
+    Player player = (Player) sender
+    
+    if (cmd.getName().equalsIgnorCase("uhc) {
+      if (args.legnth == 0) {
+        player.sendMessage(ChatColor.GRAY + "UHC Help: ");
+        player.sendMessage(ChatColor.GREEN + "/uhc - Shows The help menu" )
+        //TODO: Add More Messages
+      }
+      
+      return true;
+    }
+    
   }
   
 }
