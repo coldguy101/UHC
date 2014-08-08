96d3d62f7063053bc3c5056651d122c3a62f8ba0
CrazymanJR <adilkarim101@gmail.com>
2014-08-08 11:16:16 -0700

    Update CommandManager.java

--- src/net/UltraHardCore/Commands/CommandManager.java
+++ src/net/UltraHardCore/Commands/CommandManager.java
@@ -1,1 +1,26 @@
-//TODO: Add Code
+package net.UltraHardCore.Commands.CommandManager;
+
+import java.util.HashMap;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+public class CommandManager implements CommandExecutor {
+  
+  private HashMap<String, SubCommand> cmds;
+  
+  public CommandManager() {
+    this.cmds = new HashMap();
+    loadCommands();
+  }
+  
+  private void loadCommands() {
+    /*
+    Join
+    AddTeam
+    LeaveTeam
+    -Add more if needed
+    */
+  }
+  
+}
